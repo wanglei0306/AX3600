473d77dbbc0565f633a5f8e339d31a09c0b019ad
wanglei0306 <82213225+wanglei0306@users.noreply.github.com>
2022-04-04 12:38:50 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/10.10.10.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.30.1/g' package/base-files/files/bin/config_generate
 
